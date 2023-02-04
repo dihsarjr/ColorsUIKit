0b95c11ceb633f973366d878350f120e3b781593
mohammedrashid <mohammed1rashid786@gmail.com>
2023-02-04 23:54:46 +0530

    tableView

--- UIKIT-Intro2/ColorsTableVCViewController.swift
+++ UIKIT-Intro2/ColorsTableVCViewController.swift
@@ -8,13 +8,43 @@
 import UIKit
 
 class ColorsTableVCViewController: UIViewController {
+    var colors: [UIColor] = []
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        createColorsArray()
     }
     
 
+    func createColorsArray(){
+        for _ in 0..<50{
+            colors.append(createRandomColor())
+        }
+    }
+    
+    func createRandomColor() ->UIColor{
+        let randomColor = UIColor(red: CGFloat.random(in: 0...1), green: CGFloat.random(in: 0...1), blue: CGFloat.random(in: 0...1), alpha: 1)
+        return randomColor
+    }
 
+}
 
+extension ColorsTableVCViewController: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return colors.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ColorCell") else {
+            return UITableViewCell()
+        }
+        let color = colors[indexPath.row]
+        cell.backgroundColor=color
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "ColorsDetailsVC", sender: nil)
+    }
 }
